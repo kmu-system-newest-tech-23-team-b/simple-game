7a5142583de89c742781dac717f302427442d93e
wsb8618 <55951156+wsb8618@users.noreply.github.com>
2023-04-16 21:49:23 +0900

    [추가] number_game(Upgrade_version2)
    총 3라운드로 진행하여 라운드별 점수를 합한 최종 점수를 출력해주도록 업그레이드 하였습니다.

--- number_game/src/main.rs
+++ number_game/src/main.rs
@@ -2,47 +2,57 @@ use std::io;
 use rand::Rng;
 
 fn main() {
-    println!("Guess the number!");
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let mut total_score = 0;
+    let mut round = 0;
 
-    let mut tries = 0;
-    loop {
-        let mut guess = String::new();
-        println!("Please input your guess (1-100).");
+    while round < 3 {
+        round += 1;
+        println!("Round {} - Guess the number!", round);
 
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
+        let secret_number = rand::thread_rng().gen_range(1..=100);
+        let mut tries = 0;
 
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
+        loop {
+            let mut guess = String::new();
+            println!("Please input your guess (1-100).");
 
-        tries += 1;
+            io::stdin()
+                .read_line(&mut guess)
+                .expect("Failed to read line");
 
-        println!("You guessed: {}", guess);
+            let guess: u32 = match guess.trim().parse() {
+                Ok(num) => num,
+                Err(_) => continue,
+            };
 
-        match guess.cmp(&secret_number) {
-            std::cmp::Ordering::Less => println!("Too small!"),
-            std::cmp::Ordering::Greater => println!("Too big!"),
-            std::cmp::Ordering::Equal => {
-                println!("You win!");
-                let score = match tries {
-                    1 => 10,
-                    2 => 8,
-                    3 => 5,
-                    _ => 3,
-                };
-                println!("Your score is: {}", score);
+            tries += 1;
+
+            println!("You guessed: {}", guess);
+
+            match guess.cmp(&secret_number) {
+                std::cmp::Ordering::Less => println!("Too small!"),
+                std::cmp::Ordering::Greater => println!("Too big!"),
+                std::cmp::Ordering::Equal => {
+                    println!("You win!");
+                    let score = match tries {
+                        1 => 10,
+                        2 => 8,
+                        3 => 5,
+                        _ => 3,
+                    };
+                    println!("Your score is: {}", score);
+                    total_score += score;
+                    break;
+                }
+            }
+
+            if tries == 5 {
+                println!("You lose! The secret number was: {}", secret_number);
+                println!("Your score is: 0");
                 break;
             }
         }
-
-        if tries == 5 {
-            println!("You lose! The secret number was: {}", secret_number);
-            println!("Your score is: 0");
-            break;
-        }
     }
+
+    println!("Your total score is: {}", total_score);
 }
